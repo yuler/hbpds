556c16ea716fad61dae704db22d63e22db7b4125
root <root@precise32.(none)>
2015-04-26 04:30:31 +0000

    banner

--- resources/views/admin/banners/edit.blade.php
+++ resources/views/admin/banners/edit.blade.php
@@ -25,45 +25,76 @@
 	      </ol>
 	    </section>
 
-	    <!-- Main content -->
-	    <section class="content">
+	   <section class="content">
 	      <!-- Main row -->
 	     	<div class="row">
 	      	  	<div class="box box-primary">
+	                <div class="box-header">
+	                  	<h3 class="box-title">新建</h3>
+	                </div><!-- /.box-header -->
 
-				
-	   		 			{!! Form::model($banner, ['route' => ['admin.banner.update', $banner->id ], 'method' => 'put']) !!}
-		                
+	                <!-- form start -->
+	                
+	                
 		                <div class="box-body">
-		                    <div class="form-group">
-		                    	{!! Form::label('name', '广告名称') !!}
-		                    	{!! Form::text('name', null, ['class' => 'form-control ', 'placeholder' => '输入广告名称']) !!}
-		                    </div>
-		                    <div class="form-group">
-		                    	{!! Form::label('target_url', '跳转页面') !!}
-		                    	{!! Form::text('target_url', null, ['class' => 'form-control', 'placeholder' => '输入跳转页面']) !!}
-		                    </div>
 
-		                    <div class="form-group">
-		                    	{!! Form::label('image_url', '主图') !!}
-		                    	{!! Form::text('image_url', null, ['class' => 'form-control','placeholder' => '上传主图']) !!}
-		                    </div>
-		                    <div class="form-group">
-		                    	{!! Form::label('enable', '是否可用') !!}
-		                      	{!! Form::select('enable', ['1' => '是','0' => '否'], null, ['class' => 'form-control']) !!}
-		                    </div>
-			                    
-			            </div><!-- /.box-body -->
+							<div class="row">
 
-			                <div class="box-footer">
-			                    <button type="submit" class="btn btn-primary">提交</button>
-			                </div>
-		                	{!! Form::close() !!}
-		            	
-	               		</div>
-	            	</div>    
-	  			</div>
-			</div>
+								<div class="col-md-6">
+   					   		 			 {!! Form::model($banner, ['route' => ['admin.banner.update', $banner->id ], 'method' => 'put']) !!}
+
+				                	<div class="form-group">
+				                    	{!! Form::label('name', '广告名称') !!}
+				                    	{!! Form::text('name', null, ['class' => 'form-control ', 'placeholder' => '输入广告名称']) !!}
+				                    </div>
+				                    <div class="form-group">
+				                    	{!! Form::label('target_url', '跳转页面') !!}
+				                    	{!! Form::text('target_url', null, ['class' => 'form-control', 'placeholder' => '输入跳转页面']) !!}
+				                    </div>
+
+				                    <div class="form-group">
+				                    	{!! Form::label('image_url', '主图') !!}
+				                    	{!! Form::text('image_url', null, ['class' => 'form-control','placeholder' => '上传主图']) !!}
+				                    </div>
+				                     <div class="form-group">
+				                    	{!! Form::label('order', '顺序') !!}
+				                    	{!! Form::text('order', null, ['class' => 'form-control','placeholder' => '输入序号']) !!}
+				                    </div>
+				                    <div class="form-group">
+				                    	{!! Form::label('enable', '是否可用') !!}
+				                      	{!! Form::select('enable', ['1' => '是','0' => '否'], null, ['class' => 'form-control']) !!}
+				                    </div>
+
+				                   <button type="submit" class="btn btn-primary">提交</button>
+									{!! Form::close() !!}
+								</div><!-- end md-6-->
+
+								<div class="col-sm-6">
+								    <div class="thumbnail">
+								      
+								      @if(isset($banner['image_url']))
+								      		<img id="yulan" src="{{$banner->image_url}}" >
+								      	@else
+								      		<img id="yulan" data-src="holder.js/100%x300" >
+								      	@endif
+
+								      <div class="caption">
+								      	<form action="/admin/attachment" id="uploadAvatarForm" method="post">
+			                        			
+											<a id="dianji" href="javascript:void(0);" class="btn btn-primary" onclick="$('#uploadAvatar').trigger('click');">上传宣传图</a>
+											<input name="upload" type="file" class="hide" id="uploadAvatar">
+										</form>
+								      </div>
+								    </div>
+								</div>
+
+			                 </div><!--end box-->
+
+		                </div><!-- /.box-body -->
+
+		             
+	                
+
               	</div><!-- /.box -->
 	    	</div><!-- /.row (main row) -->
 	    </section><!-- /.content -->
@@ -71,6 +102,52 @@
 	<br>
 	<br>
 
+<script type="text/javascript">
+$('#uploadAvatar').fileupload({
+        url: '/admin/attachment?_token={{ Session::token() }}',
+        dataType: 'json',
+        forceIframeTransport: true,
+        progress: function (e, data) {
+        	$('#uploadAvatarProgress').fadeIn(1000);
+    		var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#uploadAvatarProgress .progress-bar').css(
+                'width',
+                progress + '%'
+            );
+        },
+        success: function(result, textStatus, jqXHR){
 
+		$("#image_url").val(result.file_path);
+		$("#yulan").attr('src',result.file_path);
+		        },
+        error: function (jqXHR, textStatus, errorThrown) {
+        	console.log(jqXHR);
+        	console.log(textStatus);
+        	console.log(errorThrown);
+        },
+        complete: function(e,data) {
+        	setTimeout( function(){
+    			$('#uploadAvatarProgress').fadeOut(1000);
+    			setTimeout(function(){
+    				$('#uploadAvatarProgress .progress-bar').css(
+	                    'width',
+	                    '0%'
+	                )
+    			},1000)
+    		},2000);
+        },
+    })
+	/*$(document).on('change','#uploadAvatar',function(){
+		$.ajax({
+			url :'/admin/attachment?_token={{ Session::token() }}',
+			method:'Post',
+			dateType:'JSON',
+			data: $('#uploadAvatarForm').serialize(),
+			success:function(data){
+				console.log(data);
+			}
+		})
+	});*/
+</script>
 
 @endsection
