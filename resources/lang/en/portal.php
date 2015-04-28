6cb2ae4f66e6e4cab07f7a336a8ebbd82f0af1b0
joyful <joyful4yu@gmail.com>
2015-04-28 22:45:34 +0800

    'commit'

---
+++ resources/lang/en/portal.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Header Language Lines
+	|--------------------------------------------------------------------------
+	|
+	*/
+	'header' => [
+
+		'search' => 'Product name or LOT number',
+
+		'home' => 'Home',
+
+		'auction-calendar' => 'Auction Calendar',
+		'auction-preview' => 'Auction Preview',
+		'auction-history' => 'Auction History',
+
+		'auction-online' => 'Auction Online',
+
+		'auction-notice' => 'Auction Notice',
+		'buy-service' => 'Buy Service',
+		'auction-guide' => 'Auction Guide',
+		'transaction-read' => 'Transaction Read',
+		'auction-book' => 'Auction Book',
+		
+	],
+];
