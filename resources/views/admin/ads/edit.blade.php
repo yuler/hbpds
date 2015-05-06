78540ebff0765e0fb6f02085c19bdb6a82f085b8
joyful4yu <joyful4yu@gmail.com>
2015-05-06 13:44:50 +0800

    fix cdn link

--- resources/views/admin/ads/edit.blade.php
+++ resources/views/admin/ads/edit.blade.php
@@ -103,8 +103,8 @@
 	</div><!-- /.content-wrapper -->
 	<br>
 	<br>
-<link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
-<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
+<link href="/cdn/select2.min.css" rel="stylesheet" />
+<script src="/cdn/select2.min.js"></script>
 <script type="text/javascript">
   $('select').select2();
 </script>
