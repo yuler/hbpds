e387739117fde86d39af199358754a76b4dbba7a
joyful <joyful4yu@gmail.com>
2015-05-12 21:06:18 +0800

    'commit'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,6 @@
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -56,8 +55,9 @@
 
 	@yield('content')
 
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<!-- jQuery -->
+	<script src="/plugins/jQuery/jQuery-2.1.3.min.js"></script>
+	<!-- Bootstrap JavaScript -->
+	<script src="/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
